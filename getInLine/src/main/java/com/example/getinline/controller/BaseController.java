1ebe5ba94e08e4ad8954cafb3b4b2498b66df60d
YOUNJAE <coyojo@naver.com>
2025-01-14 11:40:22 +0900

    controller
    5가지 뷰 컨트롤러 작성

--- getInLine/src/main/java/com/example/getinline/controller/BaseController.java
+++ getInLine/src/main/java/com/example/getinline/controller/BaseController.java
@@ -1,14 +1,21 @@
 package com.example.getinline.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller   // view 관련된를 만들거니까 Controller 어노테이션 붙여주기
-public class BaseController {
+public class BaseController implements ErrorController {
+    //Error페이지를 커스텀한 페이지로 쓰려먼 ErrorController를 implements 해줘야한다
 
     @GetMapping("/")
     public String root(){
         return "index";
+    }
+
+    @GetMapping("/error")
+    public String error(){
+        return "error";
     }
 
 
