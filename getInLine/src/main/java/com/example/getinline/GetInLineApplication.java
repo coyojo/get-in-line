76906f81c7e53e2bf44a2cc952b9f578ca577142
YOUNJAE <coyojo@naver.com>
2025-02-17 10:52:33 +0900

    타임리프 사용법, decoupled Logic 사용
    html5모양 그대로 쓰기위해 decoupled Logic 사용

--- getInLine/src/main/java/com/example/getinline/GetInLineApplication.java
+++ getInLine/src/main/java/com/example/getinline/GetInLineApplication.java
@@ -2,7 +2,10 @@ package com.example.getinline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScans;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class GetInLineApplication {
 
